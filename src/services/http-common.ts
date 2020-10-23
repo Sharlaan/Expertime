78cacae9c7b5cff686621c47fe7d95d2ecc948a0
Raphaël <raphael.morineau@gmail.com>
2020-10-24 00:32:28 +0200

    Fix exact query params with fulltext search querying

--- src/services/http-common.ts
+++ src/services/http-common.ts
@@ -1,20 +1,33 @@
 import { API_URL } from '@/config';
 import { FetchData, QueryParams } from '@/interfaces/FetchData';
 
-const stringifyParams = <T>(params?: QueryParams<T>) => {
-  return params && Object.keys(params)
-    ? Object.entries(params).reduce((str, [key, value], index) => {
-        const isLastItem = index === Object.entries(params).length - 1;
-        return `${str}${key}=${value}${isLastItem ? '' : '&'}`;
-      }, '?')
-    : '';
+// const stringifyParams = <T>(params?: QueryParams<T>) => {
+//   return params && Object.keys(params).length
+//     ? Object.entries(params).reduce((str, [key, value], index) => {
+//         const isLastItem = index === Object.entries(params).length - 1;
+//         return `${str}${key}=${value}${isLastItem ? '' : '&'}`;
+//       }, '?')
+//     : '';
+// };
+
+// json-server docs: https://github.com/typicode/json-server#filter
+const stringifyParams = (params?: QueryParams) => {
+  if (params === undefined || !Object.keys(params).length) return '';
+
+  let queryParams = '';
+  if (params.query) queryParams += `&q=${params.query}`;
+  if (params.brandId) queryParams += `&brand.brandId=${params.brandId}`;
+  if (params.page) queryParams += `&_page=${params.page}`;
+  if (params.limit) queryParams += `&_limit=${params.limit}`;
+
+  return queryParams.replace(/^&/, '?');
 };
 
 const defaultFetchOptions = {
   headers: { 'Content-Type': 'application/json' },
 } as RequestInit;
 
-export default async function fetchData<R>({ endpoint, params, options }: FetchData<R>) {
+export default async function fetchData<R>({ endpoint, params, options }: FetchData) {
   const url = `${API_URL}${endpoint}${stringifyParams(params)}`;
   const response = await fetch(url, { ...defaultFetchOptions, ...options });
 
