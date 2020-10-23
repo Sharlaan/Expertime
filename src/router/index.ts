2c756274fe1cbfbc21570c4de36dbef3bf9d026f
Raphaël <raphael.morineau@gmail.com>
2020-10-23 16:10:00 +0200

    Define pages in routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,26 @@
-import Vue from "vue";
-import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter, { RouteConfig } from 'vue-router';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/',
+    alias: '/campaigns',
+    name: 'campaigns',
+    component: () => import('../components/CampaignsList.vue'),
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    path: '/campaigns/:id',
+    name: 'campaign-details',
+    component: () => import('../components/Campaign.vue'),
+  },
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
